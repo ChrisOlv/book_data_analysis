2253a8d3d1afbb524b6e94e7960e8d6f0c5efcb9
chris.oliveres-talan <christophe.oliveres@talan.com>
2024-10-29 17:21:43 +0100

    work

--- script_1_update_data_et_categorie.py
+++ script_1_update_data_et_categorie.py
@@ -1,3 +1,7 @@
+import time
+# Start the timer
+start_time = time.time()
+
 import subprocess
 import sys
 import sqlite3
@@ -9,6 +13,7 @@ from langchain_openai import AzureChatOpenAI
 from langchain_openai import AzureOpenAIEmbeddings
 import os
 from dotenv import find_dotenv, load_dotenv
+
 
 
 # ETAPE 1/3
@@ -47,7 +52,9 @@ print("-"*50)
 conn.close()
 
 print("="*80)
+time_step1 = time.time()
 print("Etape 1/3 : Les tables ont été chargées !")
+print(f"Temps d'exécution de l'étape 1 : {time_step1 - start_time} secondes")
 print("="*80)
 
 # ETAPE 2/3
@@ -96,8 +103,32 @@ chain_category = (
 # ETAPE 2/3 suite
 # nettoyage et préparation de df_book
 
-# enrichissement du excel source
+# enrichissement du fichier source
 df_book['oeuvre'] = df_book[['title', 'authors']].astype(str).agg(' - '.join, axis=1)
+df_book['id long'] = df_book['id'].apply(lambda x: str(x).zfill(5))
+# renaming des colonnes
+df_book = df_book.rename(
+    columns={'last_open':'Date dernière ouverture',
+             "title":"Titre",
+             "authors":"Auteurs",
+             "series":"Série",
+             "language":"Langue",
+             "total_read_pages":"total Nbr de pages lues",
+             "pages":"page"})
+df_book['Date dernière ouverture'] = pd.to_datetime(df_book['Date dernière ouverture'], unit='s').dt.date
+# créer la colonne "auteur courts"
+df_book['Auteurs courts'] = (
+    df_book['Auteurs']
+    .str.replace(r'[.,]', ' ', regex=True)  # Remplace les points et virgules par des espaces
+    .str.split()
+    .apply(lambda x: ' '.join([word for word in x if len(word) > 2]))  # Garde les mots de plus de 2 caractères
+    .str.split()
+    .apply(lambda x: ' '.join(x[:2]))  # Garde les deux premiers mots
+)
+
+# ajout  colonne "format" = "ebook"
+df_book['format'] = "ebook"
+
 
 # fonction pour créer une colonne avec un résumé de la question. Servira à gagner en lisibilité sur les visuels
 def find_category(oeuvre):
@@ -105,49 +136,72 @@ def find_category(oeuvre):
     return result
 
 df_book['categorie'] = df_book['oeuvre'].apply(find_category)
-df_book = df_book.drop(columns=['oeuvre', 'md5'])
 
+# drop la colonne highlights
+df_book = df_book.drop(columns=['highlights','oeuvre', 'md5'])
 # Exporter les tables vers des fichiers Parquet
-df_book.to_parquet('data_sources_from_python/book_table.parquet')
+df_book.to_parquet('data_sources_from_python/df_book.parquet',engine='pyarrow')
 
 # Afficher un message de confirmation
 print("="*80)
 print("Etape 2/3 : Les catégories ont été ajoutées !")
 
+time_step2 = time.time()
+print(f"Temps d'exécution de l'étape 2 : {time_step2 - time_step1} secondes")
+print(f"Temps total d'exécution : {time_step2 - start_time} secondes")
+
+
 # Afficher les premières lignes du DataFrame
 print("book avec catégories :")
-print(df_book[['title', 'authors', 'categorie']].tail(5))
+print(df_book[['Titre', 'Auteurs', 'categorie']].tail(5))
 print("="*80)
+
 
 # nettoyage et preparation de df_stat 
 
 # changer le nom de la coonne duration pour "Temps passé sur la page en seconde"
 df_page_stat_data.rename(columns={"duration":"Temps passé sur la page en seconde"},inplace=True)
 # convertir la colonne en date time
-df_page_stat_data['start_time'] = pd.to_datetime(df_page_stat_data['start_time'], unit='s')
+df_page_stat_data.rename(columns={"start_time":"heure de début"},inplace=True)
+
+df_page_stat_data['heure de début'] = pd.to_datetime(df_page_stat_data['heure de début'], unit='s')
 # créé une colonne "id long" qui reprend la colonne "id_book" en la passant sur 5 chiffres. Les premiers chiffres doivent etre des "0". par exemple "68" devient "00068"
 df_page_stat_data['id_long'] = df_page_stat_data['id_book'].apply(lambda x: str(x).zfill(5))
 # crée une colonne "Temps de lecture en minute" et "Temps de lecture en heure" qui utilise la colonne "Temps passé sur la page en seconde", divisé par 60 puis encore par 603
 df_page_stat_data['Temps de lecture en minute'] = df_page_stat_data['Temps passé sur la page en seconde'] / 60
 df_page_stat_data['Temps de lecture en heure'] = df_page_stat_data['Temps de lecture en minute'] / 60
-# ajoute une colonne "date lecture" qui reprend la date de la colonne "start_time"
-df_page_stat_data['date lecture'] = df_page_stat_data['start_time'].dt.date
-# ajoute une colonne "Heure de début de lecture" qui reprend l'heure de la colonne "start_time" 
-df_page_stat_data['Heure de début de lecture'] = df_page_stat_data['start_time'].dt.time
-# ajoute les colonnes : Heure,	Heure en décimal, Jour Précédent. basée sur la colonne start_time 
-df_page_stat_data['Heure'] = df_page_stat_data['start_time'].dt.hour
-df_page_stat_data['Heure en décimal'] = df_page_stat_data['Heure'] + df_page_stat_data['start_time'].dt.minute / 60
-df_page_stat_data['Jour Précédent'] = (df_page_stat_data['start_time'] - pd.Timedelta(days=1)).dt.date
+# ajoute une colonne "date lecture" qui reprend la date de la colonne "heure de début"
+df_page_stat_data['date lecture'] = df_page_stat_data['heure de début'].dt.date
+# ajoute une colonne "Heure de début de lecture" qui reprend l'heure de la colonne "heure de début" 
+df_page_stat_data['Heure de début de lecture'] = df_page_stat_data['heure de début'].dt.time
+# ajoute les colonnes : Heure,	Heure en décimal, Jour Précédent. basée sur la colonne heure de début 
+df_page_stat_data['Heure'] = df_page_stat_data['heure de début'].dt.hour
+df_page_stat_data['Heure en décimal'] = df_page_stat_data['Heure'] + df_page_stat_data['heure de début'].dt.minute / 60
+df_page_stat_data['Jour Précédent'] = (df_page_stat_data['heure de début'] - pd.Timedelta(days=1)).dt.date
 # ajoute une colonne "Est Consécutif" 
 df_page_stat_data['Est Consécutif'] = (df_page_stat_data['date lecture'].shift(1) == df_page_stat_data['date lecture']) | (df_page_stat_data['date lecture'].shift(1) == df_page_stat_data['date lecture'] - pd.Timedelta(days=1))
+df_page_stat_data['date de fin de lecture'] = df_page_stat_data.groupby('id_book')['date lecture'].transform('max')
+
 
 
 # Afficher un message de confirmation
 print("="*80)
 print("Etape 3/3 : Les stats data ont été nettoyées !")
-df_page_stat_data.to_parquet('data_sources_from_python/stats_lecture.parquet')
+
+df_page_stat_data.to_parquet('data_sources_from_python/stats_lecture.parquet', engine='pyarrow')
 
 # Afficher les premières lignes du DataFrame
 print("stats data :")
 print(df_page_stat_data.tail(10))
+print("="*80)
+
+# End the timer
+end_time = time.time()
+
+# Calculate and print the execution time
+execution_time = end_time - start_time
+print("="*80)
+
+print(f"Temps d'exécution de l'étape 3 : {end_time - time_step2} secondes")
+print(f"Execution time total : {execution_time} seconds")
 print("="*80)
