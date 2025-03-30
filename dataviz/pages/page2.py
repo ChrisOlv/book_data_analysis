ac0919772113701bc773b3c5cb966223e1bb76d5
LVPortfolio <oliveres.christophe@gmail.com>
2025-03-30 22:25:05 +0200

    work

--- dataviz/pages/page2.py
+++ dataviz/pages/page2.py
@@ -8,12 +8,13 @@ import matplotlib.dates as mdates
 from datetime import timedelta
 import random
 import numpy as np
+import os
 try:
     from menu import menu_with_redirect
 except ImportError as e:
     print("ImportError:", e)
 
-
+os.chdir(os.path.dirname(__file__))
 st.set_page_config(
     page_title="page2",
     page_icon="📖",
@@ -24,8 +25,25 @@ st.set_page_config(
 # title
 # dashboard title
 st.title("📚 Book data analysis - zoom")
-st.subheader("Logs analysis from KO e-reader")
-st.markdown("""This dashboard presents an analysis of e-book reading data.
-            The data comes from the reading logs of an e-reader using Ko-reader.
+st.subheader("Comparison view")
+st.markdown("""Comparison view
 
             """)
+
+
+
+@st.cache_data
+def load_data():
+    df_book_updated = pd.read_parquet("../data_sources_from_python/df_book_updated.parquet")
+    df_stat = pd.read_parquet("../data_sources_from_python/stats_lecture.parquet")
+    df_book_paper = pd.read_excel("../paper_audio/paper_audio.xlsx")
+    
+    # Conversion des dates
+    df_book_updated['Date de lecture'] = pd.to_datetime(df_book_updated['Date de lecture'], format="%Y-%m-%dT%H:%M:%S.%fZ")
+    df_book_paper['date de lecture'] = pd.to_datetime(df_book_paper['date de lecture'], format='%d/%m/%Y')
+    
+    return df_book_updated, df_book_paper, df_stat
+
+df_book_updated, df_book_paper, df_stat = load_data()
+
+
